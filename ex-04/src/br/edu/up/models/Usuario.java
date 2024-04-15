fd4f43abe933095de740de30b86da1c0c7ac48e7
Victor Martins <VictorMLMartins@icloud.com>
2024-04-15 11:28:14 -0300

    fix: :bug: Encapsulamento e Get's e Set's

--- ex-04/src/br/edu/up/models/Usuario.java
+++ ex-04/src/br/edu/up/models/Usuario.java
@@ -1,6 +1,19 @@
 package br.edu.up.models;
 
 public class Usuario {
-  String nome;
-  double qtdDolares;
+  private String nome;
+  private double qtdDolares;
+
+  public String getNome() {
+    return nome;
+  }
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+  public double getQtdDolares() {
+    return qtdDolares;
+  }
+  public void setQtdDolares(double qtdDolares) {
+    this.qtdDolares = qtdDolares;
+  }
 }
