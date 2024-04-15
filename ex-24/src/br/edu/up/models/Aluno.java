4a55c55a89d7d26347be1c3b583343f32e9b1d11
Victor Martins <VictorMLMartins@icloud.com>
2024-04-15 10:30:24 -0300

    feat: :sparkles: Criação de Classes Modelos
    Adicionado as classes modelos do exercício 24

---
+++ ex-24/src/br/edu/up/models/Aluno.java
@@ -0,0 +1,7 @@
+package br.edu.up.models;
+
+public class Aluno {
+  String nome;
+  String curso;
+  Boletim boletim;
+}
---
+++ ex-24/src/br/edu/up/models/Boletim.java
@@ -0,0 +1,7 @@
+package br.edu.up.models;
+
+public class Boletim {
+  double trabalhoLab = 2;
+  double avSemestral = 3;
+  double exameFinal = 5;
+}
