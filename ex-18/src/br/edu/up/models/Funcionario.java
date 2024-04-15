bf57d592bbfe107fbcf529784f5e4735383a4600
Victor Martins <VictorMLMartins@icloud.com>
2024-04-15 11:29:46 -0300

    fix: :bug: Encapsulamento e Get's e Set's

--- ex-18/src/br/edu/up/models/Funcionario.java
+++ ex-18/src/br/edu/up/models/Funcionario.java
@@ -1,8 +1,33 @@
 package br.edu.up.models;
 
 public class Funcionario {
-  String nome;
-  int idade;
-  String genero;
-  double salario;
+  private String nome;
+  private int idade;
+  private String genero;
+  private double salario;
+  
+  public String getNome() {
+    return nome;
+  }
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+  public int getIdade() {
+    return idade;
+  }
+  public void setIdade(int idade) {
+    this.idade = idade;
+  }
+  public String getGenero() {
+    return genero;
+  }
+  public void setGenero(String genero) {
+    this.genero = genero;
+  }
+  public double getSalario() {
+    return salario;
+  }
+  public void setSalario(double salario) {
+    this.salario = salario;
+  }
 }
