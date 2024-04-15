b12d39ad0cbdee8386869a829e941aae25715d9b
Victor Martins <VictorMLMartins@icloud.com>
2024-04-15 11:30:07 -0300

    fix: :bug: Encapsulamento e Get's e Set's

--- ex-22/src/br/edu/up/models/Pessoa.java
+++ ex-22/src/br/edu/up/models/Pessoa.java
@@ -1,6 +1,19 @@
 package br.edu.up.models;
 
 public class Pessoa {
-  String nome;
-  Residencia residencia;
+  private String nome;
+  private Residencia residencia;
+  
+  public String getNome() {
+    return nome;
+  }
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+  public Residencia getResidencia() {
+    return residencia;
+  }
+  public void setResidencia(Residencia residencia) {
+    this.residencia = residencia;
+  }
 }
--- ex-22/src/br/edu/up/models/Residencia.java
+++ ex-22/src/br/edu/up/models/Residencia.java
@@ -1,7 +1,26 @@
 package br.edu.up.models;
 
 public class Residencia {
-  String tipo;
-  String endereco;
-  double kwH;
+  private String tipo;
+  private String endereco;
+  private double kwH;
+  
+  public String getTipo() {
+    return tipo;
+  }
+  public void setTipo(String tipo) {
+    this.tipo = tipo;
+  }
+  public String getEndereco() {
+    return endereco;
+  }
+  public void setEndereco(String endereco) {
+    this.endereco = endereco;
+  }
+  public double getKwH() {
+    return kwH;
+  }
+  public void setKwH(double kwH) {
+    this.kwH = kwH;
+  }
 }
