ba281c3a640bb394901353cc75df647e9535b518
Victor Martins <VictorMLMartins@icloud.com>
2024-04-15 11:30:17 -0300

    fix: :bug: Encapsulamento e Get's e Set's

--- ex-24/src/br/edu/up/models/Aluno.java
+++ ex-24/src/br/edu/up/models/Aluno.java
@@ -1,7 +1,26 @@
 package br.edu.up.models;
 
 public class Aluno {
-  String nome;
-  String curso;
-  Boletim boletim;
+  private String nome;
+  private String curso;
+  private Boletim boletim;
+  
+  public String getNome() {
+    return nome;
+  }
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+  public String getCurso() {
+    return curso;
+  }
+  public void setCurso(String curso) {
+    this.curso = curso;
+  }
+  public Boletim getBoletim() {
+    return boletim;
+  }
+  public void setBoletim(Boletim boletim) {
+    this.boletim = boletim;
+  }
 }
--- ex-24/src/br/edu/up/models/Boletim.java
+++ ex-24/src/br/edu/up/models/Boletim.java
@@ -1,7 +1,26 @@
 package br.edu.up.models;
 
 public class Boletim {
-  double trabalhoLab = 2;
-  double avSemestral = 3;
-  double exameFinal = 5;
+  private double trabalhoLab = 2;
+  private double avSemestral = 3;
+  private double exameFinal = 5;
+  
+  public double getTrabalhoLab() {
+    return trabalhoLab;
+  }
+  public void setTrabalhoLab(double trabalhoLab) {
+    this.trabalhoLab = trabalhoLab;
+  }
+  public double getAvSemestral() {
+    return avSemestral;
+  }
+  public void setAvSemestral(double avSemestral) {
+    this.avSemestral = avSemestral;
+  }
+  public double getExameFinal() {
+    return exameFinal;
+  }
+  public void setExameFinal(double exameFinal) {
+    this.exameFinal = exameFinal;
+  }
 }
