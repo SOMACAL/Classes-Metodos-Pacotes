b9d7d4066bfbb7f2f4214d3fa701b3f772d26575
Somacal <crdacostacaio@gmail.com>
2024-04-15 10:39:40 -0300

    criação  de classes modelos exercicio 5

---
+++ ex-05/src/models/Loja.java
@@ -0,0 +1,8 @@
+package models;
+//A Loja Mamão com Açúcar está vendendo seus produtos em 5 (cinco) prestações sem juros.
+
+//Faça um programa que receba um valor de uma compra e mostre o valor das prestações.
+
+public class Loja {
+private String 
+}
