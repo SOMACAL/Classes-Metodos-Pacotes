f5df18c8ae8cec155c18b55d7d1da6193dd36ed1
Victor Martins <VictorMLMartins@icloud.com>
2024-04-15 11:30:36 -0300

    fix: :bug: Encapsulamento e Get's e Set's

--- ex-26/src/br/edu/up/models/Cliente.java
+++ ex-26/src/br/edu/up/models/Cliente.java
@@ -1,7 +1,26 @@
 package br.edu.up.models;
 
 public class Cliente {
-  String nome;
-  int idade;
-  GrupoDeRisco GrupoDeRisco;
+  private String nome;
+  private int idade;
+  private GrupoDeRisco GrupoDeRisco;
+  
+  public String getNome() {
+    return nome;
+  }
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+  public int getIdade() {
+    return idade;
+  }
+  public void setIdade(int idade) {
+    this.idade = idade;
+  }
+  public GrupoDeRisco getGrupoDeRisco() {
+    return GrupoDeRisco;
+  }
+  public void setGrupoDeRisco(GrupoDeRisco grupoDeRisco) {
+    GrupoDeRisco = grupoDeRisco;
+  }
 }
--- ex-26/src/br/edu/up/models/GrupoDeRisco.java
+++ ex-26/src/br/edu/up/models/GrupoDeRisco.java
@@ -1,7 +1,26 @@
 package br.edu.up.models;
 
 public class GrupoDeRisco {
-  int baixo = 0;
-  int medio = 0;
-  int alto = 0;
+  private int baixo = 0;
+  private int medio = 0;
+  private int alto = 0;
+  
+  public int getBaixo() {
+    return baixo;
+  }
+  public void setBaixo(int baixo) {
+    this.baixo = baixo;
+  }
+  public int getMedio() {
+    return medio;
+  }
+  public void setMedio(int medio) {
+    this.medio = medio;
+  }
+  public int getAlto() {
+    return alto;
+  }
+  public void setAlto(int alto) {
+    this.alto = alto;
+  }
 }
