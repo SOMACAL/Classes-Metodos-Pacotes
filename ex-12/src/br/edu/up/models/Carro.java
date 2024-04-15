379355e1dc7c707191d58d95d624e7f05a7eadeb
Victor Martins <VictorMLMartins@icloud.com>
2024-04-15 11:29:15 -0300

    fix: :bug: Encapsulamento e Get's e Set's

--- ex-12/src/br/edu/up/models/Carro.java
+++ ex-12/src/br/edu/up/models/Carro.java
@@ -1,8 +1,33 @@
 package br.edu.up.models;
 
 public class Carro {
-  String nome;
-  String modelo;
-  double ano;
-  double valor;
+  private String nome;
+  private String modelo;
+  private double ano;
+  private double valor;
+  
+  public String getNome() {
+    return nome;
+  }
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+  public String getModelo() {
+    return modelo;
+  }
+  public void setModelo(String modelo) {
+    this.modelo = modelo;
+  }
+  public double getAno() {
+    return ano;
+  }
+  public void setAno(double ano) {
+    this.ano = ano;
+  }
+  public double getValor() {
+    return valor;
+  }
+  public void setValor(double valor) {
+    this.valor = valor;
+  }
 }
