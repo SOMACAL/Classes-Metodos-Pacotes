dd2365a21b196035cb8d4cc22ea821d5f568e13f
Victor Martins <VictorMLMartins@icloud.com>
2024-04-15 11:28:26 -0300

    fix: :bug: Encapsulamento e Get's e Set's

--- ex-06/src/br/edu/up/models/Produto.java
+++ ex-06/src/br/edu/up/models/Produto.java
@@ -1,6 +1,19 @@
 package br.edu.up.models;
 
 public class Produto {
-  String nome;
-  double precoCusto;
+  private String nome;
+  private double precoCusto;
+  
+  public String getNome() {
+    return nome;
+  }
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+  public double getPrecoCusto() {
+    return precoCusto;
+  }
+  public void setPrecoCusto(double precoCusto) {
+    this.precoCusto = precoCusto;
+  }
 }
