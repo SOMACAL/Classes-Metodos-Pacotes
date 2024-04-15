3f2a78794570e20ad9e94bf4c9adf9851505390f
Victor Martins <VictorMLMartins@icloud.com>
2024-04-15 11:29:56 -0300

    fix: :bug: Encapsulamento e Get's e Set's

--- ex-20/src/br/edu/up/models/Professor.java
+++ ex-20/src/br/edu/up/models/Professor.java
@@ -1,8 +1,33 @@
 package br.edu.up.models;
 
 public class Professor {
-  String nome;
-  int nivel;
-  double horaAula;
-  double salario = 0;
+  private String nome;
+  private int nivel;
+  private double horaAula;
+  private double salario = 0;
+  
+  public String getNome() {
+    return nome;
+  }
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+  public int getNivel() {
+    return nivel;
+  }
+  public void setNivel(int nivel) {
+    this.nivel = nivel;
+  }
+  public double getHoraAula() {
+    return horaAula;
+  }
+  public void setHoraAula(double horaAula) {
+    this.horaAula = horaAula;
+  }
+  public double getSalario() {
+    return salario;
+  }
+  public void setSalario(double salario) {
+    this.salario = salario;
+  }
 }
