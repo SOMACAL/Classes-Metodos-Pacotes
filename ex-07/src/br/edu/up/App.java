dde09541d99adf551170d3e0a21f6c86fcfac417
Victor Martins <VictorMLMartins@icloud.com>
2024-04-21 11:56:55 -0300

    feat: :sparkles: Calcular Imposto Carro
    Esse programa ele foi desenvolvido para calcular os impostos sobre o valor fixo de um carro.

--- ex-07/src/br/edu/up/App.java
+++ ex-07/src/br/edu/up/App.java
@@ -1,7 +1,22 @@
-package br.edu.up;
+package src.br.edu.up;
+
+import java.text.DecimalFormat;
+import src.br.edu.up.models.Carro;
+import src.br.edu.up.models.Console;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+	public static void main(String[] args) {
+		DecimalFormat decimalFormat = new DecimalFormat("#.##");
+		Carro carro = new Carro();
+
+		carro.setCustoFabrica(Console.readDouble("Informe o custo de fábrica do carro: "));
+
+		double valorComImposto = carro.getCustoFabrica() + carro.calcularImpostos();
+
+		double percentagemDistribuidor = carro.percentagemDistribuidor(valorComImposto);
+
+		double valorConsumidor = valorComImposto + percentagemDistribuidor;
+
+		System.out.println("\nO valor do carro foi de: R$" + carro.getCustoFabrica() + "\nO valor com imposto foi de: R$" + valorComImposto + "\nE a percentagem do distribuidor é de: R$" + decimalFormat.format(percentagemDistribuidor) + "\nO valor para o consumidor final é de: R$" +  decimalFormat.format(valorConsumidor));
+	}
 }
---
+++ ex-07/src/br/edu/up/models/Carro.java
@@ -0,0 +1,28 @@
+package src.br.edu.up.models;
+
+public class Carro {
+	private double custoFabrica;
+
+	public Carro() {
+	}
+
+	public Carro(double custoFabrica) {
+		this.custoFabrica = custoFabrica;
+	}
+
+	public double calcularImpostos() {
+		return custoFabrica * 0.45;
+	}
+
+	public double percentagemDistribuidor(double valorComImposto) {
+		return valorComImposto * 0.28;
+	}
+
+	public double getCustoFabrica() {
+		return custoFabrica;
+	}
+
+	public void setCustoFabrica(double custoFabrica) {
+		this.custoFabrica = custoFabrica;
+	}
+}
