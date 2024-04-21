411ff696b49a9b74a1fd1a4fe9479eb320476731
Victor Martins <VictorMLMartins@icloud.com>
2024-04-21 11:17:33 -0300

    feat: :sparkles: Calcular Salário
    Realizado a criação da aplicação calcular salário de um funcionário

--- ex-03/src/br/edu/up/App.java
+++ ex-03/src/br/edu/up/App.java
@@ -1,7 +1,18 @@
 package br.edu.up;
 
+import br.edu.up.models.Console;
+import br.edu.up.models.Vendedor;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+  public static void main(String[] args) throws Exception {
+		Vendedor vendedor = new  Vendedor();
+
+		vendedor.setNome(Console.readString("Digite o nome do vendendor: "));
+		vendedor.setSalarioFixo(Console.readDouble("Digite o valor do salário fixo: "));
+		vendedor.setVendas(Console.readDouble("Digite o valor total das vendas: "));
+
+		double salarioFinal = vendedor.calcularSalarioFinal();
+
+		System.out.println("O vendedor " + vendedor.getNome() + " tem um salário de R$" + vendedor.getSalarioFixo() + " e com sua comissão o saláro é de: R$" + salarioFinal);
+  }
 }
--- ex-03/src/br/edu/up/models/Vendedor.java
+++ ex-03/src/br/edu/up/models/Vendedor.java
@@ -1,22 +1,44 @@
 package br.edu.up.models;
 
 public class Vendedor {
-    private String nome;
-    private double salarioFixo;
-    private double vendas;
+	private String nome;
+	private double salarioFixo;
+	private double vendas;
 
-    public Vendedor(String nome, double salarioFixo, double vendas) {
-        this.nome = nome;
-        this.salarioFixo = salarioFixo;
-        this.vendas = vendas;
-    }
+	public Vendedor() {
+	};
 
-    public String getNome() {
-        return nome;
-    }
+	public Vendedor(String nome, double salarioFixo, double vendas) {
+		this.nome = nome;
+		this.salarioFixo = salarioFixo;
+		this.vendas = vendas;
+	}
 
-    public double calcularSalarioFinal() {
-        double comissao = vendas * 0.15;
-        return salarioFixo + comissao;
-    }
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getSalarioFixo() {
+		return salarioFixo;
+	}
+
+	public void setSalarioFixo(double salarioFixo) {
+		this.salarioFixo = salarioFixo;
+	}
+
+	public double getVendas() {
+		return vendas;
+	}
+
+	public void setVendas(double vendas) {
+		this.vendas = vendas;
+	}
+
+	public double calcularSalarioFinal() {
+		return salarioFixo + (vendas * 0.15);
+	}
 }
