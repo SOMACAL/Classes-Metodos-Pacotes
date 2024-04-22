5134fd4a46295c9bbf559dcd3a97e374594e6a4a
Victor Martins <VictorMLMartins@icloud.com>
2024-04-22 13:44:55 -0300

    feat: :sparkles: Tipo de triangulo
    Programa realizado para verificar qual o tipo de triângulo ele é verificando inclusive se é um triangulo recebendo os valores em console

--- ex-19/src/br/edu/up/App.java
+++ ex-19/src/br/edu/up/App.java
@@ -1,7 +1,18 @@
 package br.edu.up;
 
+import br.edu.up.models.Console;
+import br.edu.up.models.Triangulo;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+	public static void main(String[] args) throws Exception {
+		int triLado1 = Console.readInt("Informe a medida do lado 1 do triângulo: ");
+		int triLado2 = Console.readInt("Informe a medida do lado 2 do triângulo: ");
+		int triLado3 = Console.readInt("Informe a medida do lado 3 do triângulo: ");
+
+		Triangulo triangulo = new Triangulo(triLado1, triLado2, triLado3);
+
+		String tipoTriangulo = triangulo.verificarTipoTriangulo();
+
+		System.out.println(tipoTriangulo);
+	}
 }
