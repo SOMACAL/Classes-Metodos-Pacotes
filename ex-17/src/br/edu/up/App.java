861247942fbae6f17a70a5c9f7376d2e96095170
Victor Martins <VictorMLMartins@icloud.com>
2024-04-22 11:11:38 -0300

    feat: :sparkles: Calcular aumento salário
    Programa que realiza o cálculo de aumento de salário de acordo com o salário mínimo fornecido pelo console.

--- ex-17/src/br/edu/up/App.java
+++ ex-17/src/br/edu/up/App.java
@@ -1,7 +1,28 @@
 package br.edu.up;
 
-public class App {
-    public static void main(String[] args) throws Exception {
+import br.edu.up.models.Console;
+import br.edu.up.models.Funcionario;
 
-    }
+public class App {
+	public static void main(String[] args) throws Exception {
+		System.out.println("\n---------Programa para calcular reajuste de salário------------\n");
+		
+		double salarioMinimo = Console.readDouble("Informe o salário mínimo hoje: ");
+		String nome = Console.readString("Informe o nome do funcionário: ");
+		Double salario = Console.readDouble("Informe o salário do funcionário: ");
+
+		Funcionario funcionario = new Funcionario(nome, salario);
+
+		if(salario > salarioMinimo) {
+			System.out.println("\n---------------------\n");
+			System.out.println("Nome do funcionário: " + funcionario.getNome() + "\nSalário do funcionário: " + funcionario.getSalario());
+			System.out.println("\nFuncionário não apto a receber aumento.");
+		} else {
+			System.out.println("\n---------------------\n");
+			System.out.println("Nome do funcionário: " + funcionario.getNome() + "\nSalário atual do funcionário: " + funcionario.getSalario() + "\nSalário ajustado: " + salarioMinimo);
+
+			double qdtAumento = salarioMinimo - funcionario.getSalario();
+			System.out.println("\nAumento de: " + qdtAumento);
+		}
+	}
 }
--- ex-17/src/br/edu/up/models/Funcionario.java
+++ ex-17/src/br/edu/up/models/Funcionario.java
@@ -1,35 +1,29 @@
 package br.edu.up.models;
 
 public class Funcionario {
-    private String nome;
-    private double salario;
-    private double salarioMinimo;
+	private String nome;
+	private double salario;
 
-    public Funcionario(String nome, double salario, double salarioMinimo) {
-        this.nome = nome;
-        this.salario = salario;
-        this.salarioMinimo = salarioMinimo;
-    }
+	public Funcionario(String nome, double salario) {
+		this.nome = nome;
+		this.salario = salario;
+	}
 
-    public String getNome() {
-        return nome;
-    }
+	public String getNome() {
+		return nome;
+	}
 
-    public double getSalario() {
-        return salario;
-    }
+	public double getSalario() {
+		return salario;
+	}
 
-    public double getSalarioMinimo() {
-        return salarioMinimo;
-    }
+	public double calcularReajuste() {
+		double novoSalario = salario + (salario * 0.1);
+		return novoSalario;
+	}
 
-    public double calcularReajuste() {
-        double novoSalario = salario + (salario * 0.1); // Aumento de 10%
-        return novoSalario;
-    }
-
-    public double calcularAumentoFolha() {
-        double aumento = calcularReajuste() - salario;
-        return aumento;
-    }
+	public double calcularAumentoFolha() {
+		double aumento = calcularReajuste() - salario;
+		return aumento;
+	}
 }
