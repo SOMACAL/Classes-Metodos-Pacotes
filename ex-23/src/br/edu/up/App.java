f83cd19dc61d8f54c8ca4c504c1e0e34c071f9a7
Victor Martins <VictorMLMartins@icloud.com>
2024-04-22 14:08:10 -0300

    feat: :sparkles: Peso ideal por gênero
    Programa realiza a leitura de dados pessoais e a partir do gênero informado calcula o peso ideal

--- ex-23/src/br/edu/up/App.java
+++ ex-23/src/br/edu/up/App.java
@@ -1,7 +1,27 @@
 package br.edu.up;
 
+import java.text.DecimalFormat;
+import br.edu.up.models.Console;
+import br.edu.up.models.Pessoa;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+	public static void main(String[] args) throws Exception {
+		DecimalFormat df = new DecimalFormat("0.##");
+		System.out.println("-----------Peso ideal de acordo com gênero-----------");
+
+		String nome = Console.readString("Informe o nome: ");
+		Double altura = Console.readDouble("Informe a altura: ");
+		int idade = Console.readInt("Informe a idade: ");
+		String genero = Console.readString("Informe o gênero 'H' (homem) ou 'M' (mulher): ").toUpperCase();
+
+		Pessoa pessoa = new Pessoa(nome, genero, altura, idade);
+		
+		double pesoIdeal = pessoa.calcularPesoIdeal();
+
+		System.out.println("Nome: " + pessoa.getNome());
+		System.out.println("Altura: " + pessoa.getAltura());
+		System.out.println("Idade: " + pessoa.getIdade());
+		System.out.println("Gênero: " + pessoa.getGenero());
+		System.out.println("Peso ideal: " + df.format(pesoIdeal));
+	}
 }
--- ex-23/src/br/edu/up/models/Pessoa.java
+++ ex-23/src/br/edu/up/models/Pessoa.java
@@ -1,63 +1,63 @@
 package br.edu.up.models;
 
 public class Pessoa {
-    private String nome;
-    private char sexo;
-    private double altura;
-    private int idade;
+	private String nome;
+	private String genero;
+	private double altura;
+	private int idade;
 
-    public Pessoa(String nome, char sexo, double altura, int idade) {
-        this.nome = nome;
-        this.sexo = sexo;
-        this.altura = altura;
-        this.idade = idade;
-    }
+	public Pessoa(String nome, String genero, double altura, int idade) {
+		this.nome = nome;
+		this.genero = genero;
+		this.altura = altura;
+		this.idade = idade;
+	}
 
-    public String getNome() {
-        return nome;
-    }
+	public String getNome() {
+		return nome;
+	}
 
-    public char getSexo() {
-        return sexo;
-    }
+	public String getGenero() {
+		return genero;
+	}
 
-    public double getAltura() {
-        return altura;
-    }
+	public double getAltura() {
+		return altura;
+	}
 
-    public int getIdade() {
-        return idade;
-    }
+	public int getIdade() {
+		return idade;
+	}
 
-    public double calcularPesoIdeal() {
-        double pesoIdeal;
-        if (sexo == 'M') {
-            if (altura <= 1.70) {
-                if (idade <= 20) {
-                    pesoIdeal = (72.7 * altura) - 58;
-                } else if (idade >= 21 && idade <= 39) {
-                    pesoIdeal = (72.7 * altura) - 53;
-                } else {
-                    pesoIdeal = (72.7 * altura) - 45;
-                }
-            } else {
-                if (idade <= 40) {
-                    pesoIdeal = (72.7 * altura) - 50;
-                } else {
-                    pesoIdeal = (72.7 * altura) - 58;
-                }
-            }
-        } else { // Sexo feminino
-            if (altura <= 1.50) {
-                if (idade >= 35) {
-                    pesoIdeal = (62.1 * altura) - 45;
-                } else {
-                    pesoIdeal = (62.1 * altura) - 44.7;
-                }
-            } else {
-                pesoIdeal = (62.1 * altura) - 45;
-            }
-        }
-        return pesoIdeal;
-    }
+	public double calcularPesoIdeal() {
+		double pesoIdeal;
+		if (genero.equals("H")) {
+			if (altura <= 1.70) {
+				if (idade <= 20) {
+					pesoIdeal = (72.7 * altura) - 58;
+				} else if (idade >= 21 && idade <= 39) {
+					pesoIdeal = (72.7 * altura) - 53;
+				} else {
+					pesoIdeal = (72.7 * altura) - 45;
+				}
+			} else {
+				if (idade <= 40) {
+					pesoIdeal = (72.7 * altura) - 50;
+				} else {
+					pesoIdeal = (72.7 * altura) - 58;
+				}
+			}
+		} else {
+			if (altura <= 1.50) {
+				if (idade >= 35) {
+					pesoIdeal = (62.1 * altura) - 45;
+				} else {
+					pesoIdeal = (62.1 * altura) - 44.7;
+				}
+			} else {
+				pesoIdeal = (62.1 * altura) - 45;
+			}
+		}
+		return pesoIdeal;
+	}
 }
