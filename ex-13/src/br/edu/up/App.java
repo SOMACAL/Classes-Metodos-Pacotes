f5234df4d35e9de64727f6863d5a54e9c3d0ea18
Victor Martins <VictorMLMartins@icloud.com>
2024-04-22 10:29:55 -0300

    feat: :sparkles: Apto ou inápto?
    Programa que realiza a verificação de aptidão de "N" pessoas lidas pelo console.

--- ex-13/src/br/edu/up/App.java
+++ ex-13/src/br/edu/up/App.java
@@ -1,7 +1,36 @@
 package br.edu.up;
 
+import java.util.ArrayList;
+import br.edu.up.models.Console;
+import br.edu.up.models.Pessoa;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+	public static void main(String[] args) throws Exception {
+		System.out.println("----------Sistema para verificar aptdão------------");
+		int qtdPessoas = Console.readInt("Informe quantas pessoas quer verificar: ");
+
+		ArrayList<Pessoa> pessoas = new ArrayList<>();
+
+		for(int i = 0; i < qtdPessoas; i++) {
+			String nome = Console.readString("Informe o nome: ");
+			int idade = Console.readInt("Informe a idade: ");
+			String genero = Console.readString("Informe o genero 'M' ou 'H'").toUpperCase();
+			String aptoEscolha = Console.readString("Pessoa encontra-se apto? 'S' para sim, 'N', para não").toUpperCase();
+			Boolean apto = aptoEscolha.equals("S")? true : false;
+
+			Pessoa pessoa =  new Pessoa(nome, genero, idade, apto);
+
+			pessoas.add(pessoa);
+		}
+
+		int qtdAptos = 0;
+
+		for (Pessoa pessoa : pessoas) {
+			if(pessoa.getSaude()) qtdAptos++;
+		}
+
+		int qtdInaptos = pessoas.size() - qtdAptos;
+
+		System.out.println("A quatidade de pessoas lidas foi de: " + qtdPessoas + ".\nDessas pessoas foram contadas " + qtdAptos + " pessoas aptas e " + qtdInaptos + " pessoas inaptas.");
+	}
 }
--- ex-13/src/br/edu/up/models/Pessoa.java
+++ ex-13/src/br/edu/up/models/Pessoa.java
@@ -1,31 +1,33 @@
 package br.edu.up.models;
 
 public class Pessoa {
-    private String nome;
-    private char sexo; // 'M' para masculino, 'F' para feminino
-    private int idade;
-    private boolean saude; // true para apto, false para não apto
+  private String nome;
+  private int idade;
+  private String genero;
+  private boolean saude;
 
-    public Pessoa(String nome, char sexo, int idade, boolean saude) {
-        this.nome = nome;
-        this.sexo = sexo;
-        this.idade = idade;
-        this.saude = saude;
-    }
+  public Pessoa() {}
 
-    public String getNome() {
-        return nome;
-    }
+  public Pessoa(String nome, String genero, int idade, boolean saude) {
+    this.nome = nome;
+    this.genero = genero;
+    this.idade = idade;
+    this.saude = saude;
+  }
 
-    public char getSexo() {
-        return sexo;
-    }
+  public String getNome() {
+    return nome;
+  }
 
-    public int getIdade() {
-        return idade;
-    }
+  public String getGenero() {
+    return genero;
+  }
 
-    public boolean isApto() {
-        return saude && idade >= 18 && idade <= 30 && sexo == 'M';
-    }
+  public int getIdade() {
+    return idade;
+  }
+
+  public boolean getSaude() {
+    return saude;
+  }
 }
