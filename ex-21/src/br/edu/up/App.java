ab1686f673446f65e444aebcfcba658b729c842f
Victor Martins <VictorMLMartins@icloud.com>
2024-04-22 13:51:42 -0300

    feat: :sparkles: Verifica Categoria de Nadador
    Programa realiza a verificação da categoria do nadador de acordo coma idade fornecida via console.

--- ex-21/src/br/edu/up/App.java
+++ ex-21/src/br/edu/up/App.java
@@ -1,7 +1,16 @@
 package br.edu.up;
 
+import br.edu.up.models.Console;
+import br.edu.up.models.Nadador;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+	public static void main(String[] args) throws Exception {
+		int idade = Console.readInt("Informe a idade do nadador: ");
+
+		Nadador nadador = new Nadador(idade);
+
+		String categoria = nadador.classificarCategoria();
+
+		System.out.println("O nadador informado se encaixa na categoria: " + categoria);
+	}
 }
